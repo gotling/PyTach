34c0c6c73a65da3120aa52600254afc909e9a3bc
Marcus Gotling <marcus@gotling.se>
2014-07-05 17:25:02 +0200

    Remove unused main and unused imports

--- pytach/wsgi.py
+++ pytach/wsgi.py
@@ -1,12 +1,9 @@
 import bottle
-from bottle import route, run
-from web import web
+
 import config
+from web import web
 
 app = application = bottle.Bottle()
 app.merge(web.app)
 
 config.arguments['--verbose'] = True
-
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8082, debug=True)
