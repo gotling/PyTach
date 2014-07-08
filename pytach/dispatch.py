1ef73b164b7a648bc57f07db8b8830f981a14d4d
Marcus Gotling <marcus@gotling.se>
2014-07-08 11:34:05 +0200

    Move activities to json configuration file

--- pytach/dispatch.py
+++ pytach/dispatch.py
@@ -5,6 +5,7 @@ import config
 import reader
 
 devices = reader.read_path("devices")
+activities = reader.read_path("activities")
 
 def log(prompt, command):
     if config.arguments['--verbose']:
@@ -18,12 +19,22 @@ def send(command):
 def get_command(device, command_name):
     if devices.has_key(device):
         command = next((item for item in devices[device]["commands"] if item["name"] == command_name), False)
-        if command == False:
-            raise NameError("Device '%s' has no command named '%s'" % (device, command_name))
-        else:
+        if command:
             return command["code"]
+        else:
+            raise NameError("Device '%s' has no command named '%s'" % (device, command_name))
     else:
         raise NameError("Device '%s' deos not exist" % device)
+
+def get_activity(group, activity_name):
+    if activities.has_key(group):
+        activity = next((item for item in activities[group]["activities"] if item["name"] == activity_name), False)
+        if activity:
+            return activity
+        else:
+            raise NameError("Activity group '%s' has no activity named '%s'" (group, activity_name))
+    else:
+        raise NameError("Activity group '%s' does not exist" % activity)
 
 def get_connection(device):
     if config.connection.has_key(device):
@@ -31,52 +42,15 @@ def get_connection(device):
     else:
         raise NameError("No connection configured for device '%s'" % name)
 
-def device(name, command_name):
-    connection = get_connection(name)
-    sub_command = get_command(name, command_name)
+def device(device_name, command_name):
+    connection = get_connection(device_name)
+    sub_command = get_command(device_name, command_name)
     command = itach.build_command(1, connection, sub_command) 
 
     send(command)
 
-def activity(name, command):
-    if name == 'watch':
-        if command == 'ps3':
-            sub_command = epson.get_command('power')
-            command = itach.build_command(1, 2, sub_command)
-            send(command)
+def activity(activity_group, activity_name):
+    activity = get_activity(activity_group, activity_name)
+    for command in activity["commands"]:
+        device(command["device"], command["command"])
 
-            sub_command = nexa.build_command(1, "off")
-            command = itach.build_command(1, 1, sub_command)
-            send(command)
-
-            sub_command = multibrackets.get_command('down')
-            command = itach.build_command(1, 1, sub_command)
-            send(command)
-
-            sub_command = yamaha.get_command('dtv')
-            command = itach.build_command(1, 3, sub_command)
-            send(command)
-        elif command == 'stop':
-            sub_command = epson.get_command('power')
-            command = itach.build_command(1, 2, sub_command)
-            send(command)
-
-            sub_command = epson.get_command('power')
-            command = itach.build_command(1, 2, sub_command)
-            send(command)
-
-            sub_command = nexa.build_command(1, "on")
-            command = itach.build_command(1, 1, sub_command)
-            send(command)
-
-            sub_command = multibrackets.get_command('up')
-            command = itach.build_command(1, 1, sub_command)
-            send(command)
-
-            sub_command = yamaha.get_command('cd')
-            command = itach.build_command(1, 3, sub_command)
-            send(command)
-        else:
-            raise NameError("The watch cammand '%s' does not exist" % command)
-    else:
-        raise NameError("The activity '%s' does not exist" % name)
