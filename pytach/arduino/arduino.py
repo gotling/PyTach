58adc44dc1fdfeee96507db4080e42b4c519acd7
Marcus Gotling <marcus@gotling.se>
2014-07-09 23:55:37 +0200

    Use dispatch in arduino read

--- pytach/arduino/arduino.py
+++ pytach/arduino/arduino.py
@@ -5,6 +5,8 @@ import serial
 import config
 import time
 
+import dispatch
+
 port = '/dev/tty.usbmodemfd131'
 ser = serial.Serial(port, 9600)
 
@@ -21,6 +23,12 @@ def read():
         while True:
             line = ser.readline().strip()
             print "<", line
+            if (len(line.split()) == 3):
+                switch, group, command = line.split()
+                if switch == "device":
+                    dispatch.device(group, command);
+                elif switch == "activity":
+                    dispatch.activity(group, command)
     except KeyboardInterrupt:
         print "Terminated by user"
     finally:
